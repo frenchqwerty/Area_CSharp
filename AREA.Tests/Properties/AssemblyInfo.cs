476396d4446cf09b9f90a2193ee3a9c3bce0bf6f
Yassir <yassir.jabbari@epitech.eu>
2017-11-27 16:01:36 +0100

    INIT COMMIT

---
+++ AREA.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de 
+// l’ensemble d’attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("AREA.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AREA.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L’affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l’attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l’ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("98f8e101-c58e-483c-8ac1-65c42d86fd42")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version mineure
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou vous pouvez définir par défaut les valeurs Révision et Numéro de version
+// en utilisant ’*’, comme indiqué ci-dessous :
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AREA/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using AREA.Models;
+using Facebook;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AREA.Controllers
+{
+    [RequireHttps]
+    public class HomeController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            string CurrentUserId = Environment.GetEnvironmentVariable("UserId");
+            ViewBag.userId = CurrentUserId;
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult GetFacebookAccess()
+        {
+            return Redirect("https://www.facebook.com/v2.11/dialog/oauth?client_id=1867059553604361&redirect_uri=https://localhost:44303/Home/GetAccessToken&response_type=token&scope=public_profile,publish_actions");
+        }
+
+        [Authorize]
+        public ActionResult GetAccessToken()
+        {
+            var context = System.Web.HttpContext.Current;
+            var access_token = context.Request["access_token"];
+
+            System.Diagnostics.Debug.WriteLine("aaaaaaaaaaaaaaaaaaaaèèèèèèèèèèèèèèèèèèèèèèèèèè---------------------------------");
+            System.Diagnostics.Debug.WriteLine(access_token);
+            return Redirect("/");
+        }
+    }
+}
